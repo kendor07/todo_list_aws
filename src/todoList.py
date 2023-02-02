4b5d03b6b46873b86e494b08c65c6410d4eb409a
kendor07 <59501880+kendor07@users.noreply.github.com>
2023-02-02 07:46:23 +0100

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -17,8 +17,7 @@ def get_table(dynamodb=None):
             boto3.client = functools.partial(boto3.client,
                                              endpoint_url=URL)
             boto3.resource = functools.partial(boto3.resource)
-        dynamodb = boto3.resource("dynamodb",
-                                 REGION_NAME)
+        dynamodb = boto3.resource("dynamodb",REGION_NAME)
 
     # region_name=REGION_NAME)
     # fetch todo from the database
