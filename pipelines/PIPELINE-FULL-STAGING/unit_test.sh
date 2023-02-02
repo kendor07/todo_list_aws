115d4623a73c349ff508b51c7a2c18479f1298f0
kendor07 <59501880+kendor07@users.noreply.github.com>
2023-02-02 01:17:27 +0100

    Update unit_test.sh

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -5,7 +5,7 @@ set -x
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 echo "PYTHONPATH: $PYTHONPATH"
 export DYNAMODB_TABLE=todoUnitTestsTable
-export ENDPOINT_OVERRIDE="http://localhost:8000"
+#export ENDPOINT_OVERRIDE="http://localhost:8000"
 #export REGION_NAME=us-east-1
 python test/unit/TestToDo.py
 pip show coverage
