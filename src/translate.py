e7fcef6ed79795bb944bb6b49511fe33dc99e640
Ubuntu <ubuntu@ip-172-31-6-227.ec2.internal>
2023-02-02 09:23:27 +0000

    fix lambda translate

--- src/translate.py
+++ src/translate.py
@@ -6,8 +6,9 @@ import boto3
 
 def translate(event, context):
     # create a response
+    print(event)
     item = todoList.get_item(event['pathParameters']['id'])
-    lang = event['pathParameters']['id']['lang']
+    lang = event['pathParameters']['lang']
     if item:
         translate = boto3.client(service_name='translate',
                                  region_name='us-east-1',
