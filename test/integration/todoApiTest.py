60f47f5b4e1c4a2a5bd778696440c1b0684d459b
Ubuntu <ubuntu@ip-172-31-6-227.ec2.internal>
2023-02-02 14:01:38 +0000

    fix integration test

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -219,23 +219,15 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.status_code, 200, "Error en la petición API a {url}"
         )
-        #Delete TODO to restore state
-        response = requests.delete(url)
-        self.assertEqual(
-            response.status_code, 200, "Error en la petición API a {url}"
-        )
-        #Test TRANSLATE TODO
+        # Delete TODO to restore state
+        # response = requests.delete(url)
+        # self.assertEqual(
+        #    response.status_code, 200, "Error en la petición API a {url}"
+        # )
+        # Test TRANSLATE TODO
         url = BASE_URL+"/todos/"+ID_TODO+"/es"
         response = requests.get(url)
-        print("--response--")
-        print(response)
-        print("------------")
         json_response = response.json()
-        print('response json '+ str(json_response))
-        print("------------")
-        #jsonbody= json.loads(json_response['body'])
-        #ID_TODO = jsonbody['id']
-        #print ('ID todo:'+ID_TODO)
         self.assertEqual(
             json_response['ResponseMetadata']['HTTPStatusCode'], 200, "Error en la petición API a {url}"
         )
@@ -243,5 +235,3 @@ class TestApi(unittest.TestCase):
             json_response['TranslatedText'], "Ejemplo de texto de integración: GET", "Error en la petición API a {url}"
         )
         print('End - integration test Get TODO')
-    
-    
