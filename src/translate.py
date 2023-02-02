18f5f83e0ffa673c95f86f9338d2a11593212f0a
Ubuntu <ubuntu@ip-172-31-6-227.ec2.internal>
2023-02-02 08:20:31 +0000

    testing lambda

---
+++ src/translate.py
@@ -0,0 +1,28 @@
+import json
+import decimalencoder
+import todoList
+import boto3
+
+def translate(event, context):
+    # create a response
+    item = todoList.get_item(event['pathParameters']['id'])
+    lang = todoList.get_item(event['pathParameters']['id']['lang'])
+    if item:
+        translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+        result = translate.translate_text(Text=item.text, 
+            SourceLanguageCode="en", TargetLanguageCode=lang)
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(result,
+                               cls=decimalencoder.DecimalEncoder)
+        }
+    else:
+        response = {
+            "statusCode": 404,
+            "body": ""
+        }
+    return response
+
+print('TranslatedText: ' + result.get('TranslatedText'))
+print('SourceLanguageCode: ' + result.get('SourceLanguageCode'))
+print('TargetLanguageCode: ' + result.get('TargetLanguageCode'))
